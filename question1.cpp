41758e890535cf3cf1891e877e64e5924b429359
Aniket Tiwari <86273548+anitiwari2003@users.noreply.github.com>
2024-05-20 11:52:55 -0700

    Create question1.cpp

---
+++ question1.cpp
@@ -0,0 +1,12 @@
+/*Write a program to display the following output using a single cout statement.
+Maths=70, Physics=64, Chemistry=86*/
+#include<iostream> 
+using namespace std;
+int main()
+{
+    int physics=64,chemistry=86,maths=70;
+    cout<<"\nmaths="<<maths;
+    cout<<"\nchemistry="<<chemistry;
+    cout<<"\nphysics="<<physics;
+    return 0;
+}
